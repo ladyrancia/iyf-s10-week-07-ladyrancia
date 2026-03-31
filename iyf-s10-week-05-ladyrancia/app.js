aa5e62ea14b14d7ccee36f30d4f33c4ac5c63891
ladyrancia <ladyrancia@gmail.com>
2026-04-01 00:10:02 +0300

    setup: connected app.js

---
+++ iyf-s10-week-05-ladyrancia/app.js
@@ -0,0 +1,4 @@
+console.log("Todo app running");
+const form = document.getElementById("todo-form");
+const input = document.getElementById("todo-input");
+const list = document.getElementById("todo-list");
