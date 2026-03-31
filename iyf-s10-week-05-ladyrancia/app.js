36f40340cf6a159ed2c12279ee4ff37334c60aea
ladyrancia <ladyrancia@gmail.com>
2026-04-01 00:13:03 +0300

    feat: add new todo items

--- iyf-s10-week-05-ladyrancia/app.js
+++ iyf-s10-week-05-ladyrancia/app.js
@@ -2,3 +2,16 @@ console.log("Todo app running");
 const form = document.getElementById("todo-form");
 const input = document.getElementById("todo-input");
 const list = document.getElementById("todo-list");
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const text = input.value.trim();
+    if (text === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    list.appendChild(li);
+
+    input.value = "";
+});
