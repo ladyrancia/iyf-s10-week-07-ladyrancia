eec02bd5103562960bb5d5093a96de5c406fccd0
ladyrancia <ladyrancia@gmail.com>
2026-04-01 00:14:12 +0300

    feat: added delete button to todos

--- iyf-s10-week-05-ladyrancia/app.js
+++ iyf-s10-week-05-ladyrancia/app.js
@@ -15,3 +15,20 @@ form.addEventListener("submit", function(e) {
 
     input.value = "";
 });
+form.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    const text = input.value.trim();
+    if (text === "") return;
+
+    const li = document.createElement("li");
+    li.textContent = text;
+
+    const btn = document.createElement("button");
+    btn.textContent = "X";
+
+    li.appendChild(btn);
+    list.appendChild(li);
+
+    input.value = "";
+});
